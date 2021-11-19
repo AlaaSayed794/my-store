d2bc49e5c34ce5b7b82d80f5e37460e54dd16b57
AlaaSayed794 <alaa.elsayed.aly@gmail.com>
2021-11-19 23:31:57 +0200

    add cart service

---
+++ src/app/cart.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Product } from './types/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  cart: Product[]
+
+  constructor() {
+    this.cart = [];
+  }
+
+  addToCart(newProduct: Product): Product[] {
+    const product: Product = this.cart.find((p: Product) => p.id === product.id) as Product
+    if (product) {
+      this.cart = this.cart.map((p: Product) => {
+        if (p.id == newProduct.id) {
+          p.amount = p.amount + newProduct.amount
+        }
+        return p
+      })
+    }
+    else {
+      this.cart.push(newProduct);
+    }
+    return this.cart;
+  }
+
+  removeFromCart(product: Product): Product[] {
+    this.cart = this.cart.filter(p => p.id === product.id)
+    return this.cart
+  }
+
+  resetCart(): Product[] {
+    this.cart = []
+    return this.cart
+  }
+
+
+}
